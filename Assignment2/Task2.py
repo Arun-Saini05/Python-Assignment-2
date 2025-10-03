dc71b5a2f5dce4ea649dc5dd76879e9ee2e6c304
Arun-Saini05 <arunsaini20051223@gmail.com>
2025-10-03 22:54:16 +0530

    Update Task2.py

--- Assignment2/Task2.py
+++ Assignment2/Task2.py
@@ -5,7 +5,7 @@ total_sum = 0
 
 # Loop through numbers from 1 to 50
 for i in range(1, 51):
-    total_sum += i
+    total_sum = total_sum + i
 
 # Display the result
 print("The sum of integers from 1 to 50 is:", total_sum)
