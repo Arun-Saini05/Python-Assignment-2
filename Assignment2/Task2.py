59180d95d4d7061a96b5590d410af198836020d0
Arun-Saini05 <arunsaini20051223@gmail.com>
2025-10-03 22:51:42 +0530

    Create Task2.py

---
+++ Assignment2/Task2.py
@@ -0,0 +1,11 @@
+# Task 2: Sum of Integers from 1 to 50
+
+# Initialize sum variable
+total_sum = 0
+
+# Loop through numbers from 1 to 50
+for i in range(1, 51):
+    total_sum += i
+
+# Display the result
+print("The sum of integers from 1 to 50 is:", total_sum)
